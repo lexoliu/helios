5120a300ae4bc76a754f6a4e0424cc0f53eb4cab
Lexo Liu <me@lexo.cool>
2026-05-05 21:16:13 +0800

    index component cache identities

--- kernel/src/component_cache.rs
+++ kernel/src/component_cache.rs
@@ -1,8 +1,6 @@
 extern crate alloc;
 
 use alloc::sync::Arc;
-use alloc::vec::Vec;
-
 use bytes::Bytes;
 use lru::LruCache;
 
@@ -10,15 +8,10 @@ pub struct ComponentCache<Component> {
     budget_bytes: usize,
     resident_bytes: usize,
     entries: LruCache<Bytes, Arc<Component>>,
-    identity_entries: Vec<ComponentCacheIdentityEntry<Component>>,
+    identity_entries: LruCache<ArtifactIdentity, Arc<Component>>,
 }
 
-struct ComponentCacheIdentityEntry<Component> {
-    identity: ArtifactIdentity,
-    component: Arc<Component>,
-}
-
-#[derive(Clone, Copy, PartialEq, Eq)]
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
 struct ArtifactIdentity {
     ptr: usize,
     len: usize,
@@ -39,18 +32,14 @@ impl<Component> ComponentCache<Component> {
             budget_bytes,
             resident_bytes: 0,
             entries: LruCache::unbounded(),
-            identity_entries: Vec::new(),
+            identity_entries: LruCache::unbounded(),
         }
     }
 
     pub fn get(&mut self, artifact: &Bytes) -> Option<Arc<Component>> {
         let identity = ArtifactIdentity::from_bytes(artifact);
-        if let Some(entry) = self
-            .identity_entries
-            .iter()
-            .find(|entry| entry.identity == identity)
-        {
-            return Some(entry.component.clone());
+        if let Some(component) = self.identity_entries.get(&identity) {
+            return Some(component.clone());
         }
 
         self.entries.get(artifact.as_ref()).cloned()
@@ -75,10 +64,7 @@ impl<Component> ComponentCache<Component> {
             replaced.is_none(),
             "component cache replaced an entry after miss revalidation"
         );
-        self.identity_entries.push(ComponentCacheIdentityEntry {
-            identity,
-            component: component.clone(),
-        });
+        self.identity_entries.put(identity, component.clone());
         self.evict_to_budget();
         component
     }
@@ -89,12 +75,42 @@ impl<Component> ComponentCache<Component> {
                 panic!("component cache accounting lost track of resident bytes");
             };
             let identity = ArtifactIdentity::from_bytes(&artifact);
-            self.identity_entries
-                .retain(|entry| entry.identity != identity);
+            let _ = self.identity_entries.pop(&identity);
             self.resident_bytes = self
                 .resident_bytes
                 .checked_sub(artifact.len())
                 .expect("component cache byte accounting underflow");
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn identity_lookup_uses_pointer_key_without_content_scan() {
+        let mut cache = ComponentCache::new(1024);
+        let artifact = Bytes::from_static(b"component");
+        let component = Arc::new(7_u32);
+        let inserted = cache.insert_if_missing(artifact.clone(), component);
+
+        let hit = cache
+            .get(&artifact)
+            .expect("identity lookup should hit inserted artifact");
+        assert!(Arc::ptr_eq(&inserted, &hit));
+    }
+
+    #[test]
+    fn eviction_removes_identity_entry() {
+        let mut cache = ComponentCache::new(4);
+        let first_artifact = Bytes::from_static(b"aaaa");
+        let second_artifact = Bytes::from_static(b"bbbb");
+        let first_component = cache.insert_if_missing(first_artifact.clone(), Arc::new(1_u32));
+
+        cache.insert_if_missing(second_artifact, Arc::new(2_u32));
+
+        let replacement = cache.insert_if_missing(first_artifact, Arc::new(3_u32));
+        assert!(!Arc::ptr_eq(&first_component, &replacement));
     }
 }
